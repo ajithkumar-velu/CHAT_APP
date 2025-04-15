1ca9fc4eda3bc80b7b471429a3e7e8e6ec73c392
Ajith Kumar V <ajithajk46@gmail.com>
2025-04-15 12:07:19 +0530

    baseURL change for deploying perpose

--- Frontend/src/config/axiosInstance.js
+++ Frontend/src/config/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 const axiosInstance = axios.create({
-    baseURL: ' http://localhost:3000',
+    // baseURL: ' http://localhost:3000',
+    baseURL: 'https://chat-app-backend-la16.onrender.com',
     headers: { 'Content-Type': 'application/json'},
     withCredentials: true
 })
