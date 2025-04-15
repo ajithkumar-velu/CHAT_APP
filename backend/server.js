3ec3e19baff369ad99caddb5cb3478423646cb70
Ajith Kumar V <ajithajk46@gmail.com>
2025-04-15 12:12:24 +0530

    origin changed in backedn for deploying perpose

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,8 @@ app.use(bodyParser.urlencoded({ extended: true, limit: "6mb"}))
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    // origin: ["http://localhost:5173"],
+    origin: ["https://chat-app-frontend-qss7.onrender.com"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }))
